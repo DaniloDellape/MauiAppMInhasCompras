0abc2ebd70d37aef949bf97ba49927f9790ab5bc
DaniloDellape <danilodellape@gmail.com>
2026-03-08 12:48:07 -0300

    início da interface gráfica com inserção de dados

--- MauiAppMInhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMInhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,7 @@
+using MauiAppMInhasCompras.Models;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
 namespace MauiAppMInhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +10,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{ 
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Produto cadastrado com sucesso!", "OK");
+        }
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
